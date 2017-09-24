eb1d95d3934e98174b393bc874602a9feaf6543f
Anders Broen <anders.kjeka.broen@gmail.com>
2017-09-24 12:49:09 +0200

    add email and url

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='term_weather',
     version='0.1',
     packages=['term_weather'],
-    url='',
+    url='https://github.com/kjeka91/term_weather',
     license='',
     requires=["xmltodict", "click"],
     author='Anders Broen',
-    author_email='',
+    author_email='anders.kjeka.broen@gmail.com',
     description='Python command line tool for accessing Yr weather data',
     entry_points={
         'console_scripts': ['term_weather=term_weather.command_line:cli'],
